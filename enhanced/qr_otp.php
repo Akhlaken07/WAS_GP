94c81046bfcaf18f02a849cb18a4b93c8ef5a9b5
Akhlaken07 <148112697+Akhlaken07@users.noreply.github.com>
2024-06-24 19:27:37 +0800

    original

--- enhanced/qr_otp.php
+++ enhanced/qr_otp.php
@@ -11,7 +11,7 @@ if (isset($_POST['otp-submit'])){
         header("Location: userDetailsPage.php"); 
         exit();
     } else {
-        echo "NO \n";
+        echo "Incorrect OTP Code \n";
     }   
 }
 ?>
