7db1886afc1e198408ccbd86594b86e8afbceee2
Akhlaken07 <148112697+Akhlaken07@users.noreply.github.com>
2024-06-24 17:35:19 +0800

    otp uodate

--- enhanced/login.php
+++ enhanced/login.php
@@ -29,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Redirect based on role
         if ($user['role'] == 'admin') {
-            header("Location: userDetailsPage.php");
+            header("Location: qr_otp.php");
         } else {
             header("Location: userPage.php");
         }
--- enhanced/qr_otp.php
+++ enhanced/qr_otp.php
@@ -8,7 +8,7 @@ if (isset($_POST['otp-submit'])){
     $g = new \Sonata\GoogleAuthenticator\GoogleAuthenticator();
     
     if ($g->checkCode($secret, $code)) {
-        header("Location: database_feedback.php"); 
+        header("Location: userDetailsPage.php"); 
         exit();
     } else {
         echo "NO \n";
